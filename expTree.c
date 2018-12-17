71e31d4000c78e18b448cbb9ed3c32b6c70ba28b
Yonatan <ynnako@gmail.com>
2018-12-18 00:50:52 +0200

    1.Defined the Tree's function fields. 2.Started to implement some of the DB's functions.

--- expTree.c
+++ expTree.c
@@ -18,8 +18,45 @@ typedef struct _tree
 {
 	pNode root;
 
+	CloneFunction		clone_element;
+	DelFunction			delete_element;
+	OperateFunction		operate_on_elements;
+	GetKeyFunction		get_elements_key;
+	CompareKeyFunction	compare_keys;
 	/* *** complete the definition of the tree structure *** */
 } Tree;
 
-/* *** complete the interface functions implementation *** */
+pTree TreeCreate(CloneFunction clone_element, DelFunction delete_element, OperateFunction operate_on_elements, GetKeyFunction get_elements_key, CompareKeyFunction compare_keys)
+{
+	pTree p_new_tree;
+	//pNode p_node;
+	p_new_tree = (pTree)malloc(sizeof(Tree));
+	//p_node = (pNode)malloc(sizeof(Node));
+	if (p_new_tree == NULL/* || p_node == NULL*/) return NULL;
+	p_node->elem = NULL;
+	p_node->leftChild = NULL;
+	p_node->rightChild = NULL;
+	p_new_tree->root = p_node;
+	p_new_tree->clone_element = clone_element;
+	p_new_tree->delete_element = delete_element;
+	p_new_tree->operate_on_elements = operate_on_elements;
+	p_new_tree->get_elements_key = get_elements_key;
+	p_new_tree->compare_keys = compare_keys;
+
+	return p_new_tree;
+}
+
+void TreeDestroy(pTree p_tree)
+{			
+			return;		
+}
+
+pNode TreeAddRoot(pNode p_node, pTree p_tree)
+{
+	p_tree->root = p_tree->clone_element(p_node);//memory was allocated
+	if (p_tree->root == NULL) return NULL;
+	return p_tree->root;
+}
+
+pNode TreeAddLeftChild(pTree p_tree , pNode p_node , pElement p_elem)
 
--- expTree.h
+++ expTree.h
@@ -4,18 +4,19 @@
 #include "defs.h"
 
 /* complete the definition of the pointers to Tree and Node */
+typedef struct _node* pNode;
+typedef struct _tree* pTree;
 
 typedef void* pElement;
 typedef void* pKey;
 
 typedef pElement	(*CloneFunction)(pElement e);
 typedef void		(*DelFunction)(pElement e);
-typedef pElement	(*OperateFunction)(pElement op, 
-									   pElement left, 
+typedef pElement	(*OperateFunction)(pElement op,
+									   pElement left,
 									   pElement right);
 typedef pKey		(*GetKeyFunction)(pElement elem);
-typedef Bool		(*CompareKeyFunction)(const pKey key1, 
-										  const pKey key2);
+typedef Bool		(*CompareKeyFunction)(const pKey key1, const pKey key2);
 
 /* complete the definition of the interface functions */
 
