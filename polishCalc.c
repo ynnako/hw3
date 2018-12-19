94182737fab3fe9b4c40b28ccc4bc9302f77d5be
Yonatan <ynnako@gmail.com>
2018-12-19 15:50:36 +0200

    fixed polishcalc.c template

--- polishCalc.c
+++ polishCalc.c
@@ -5,3 +5,27 @@
 #include "polishCalcTypes.h"
 #include "defs.h"
 #include "expTree.h"
+
+
+Result InitExpression(char* exp);
+
+/* Set symbol value */
+Result SetSymbolVal(char* symName, float val);
+
+/* Evaluate expression */
+Result EvaluateExpression(float *res);
+
+/* Destroy expression */
+void DeleteExpression();
+
+pElement(*CloneFunction)(pElement e);
+
+void(*DelFunction)(pElement e);
+
+pElement(*OperateFunction)(pElement op,
+	pElement left,
+	pElement right);
+
+pKey(*GetKeyFunction)(pElement elem);
+
+Bool(*CompareKeyFunction)(const pKey key1, const pKey key2);
